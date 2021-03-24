971ec76c2ac68aa201a3a6e9aa8994d45c45e482
toosch <toon.schuurmans@gmail.com>
2021-03-24 14:41:32 +0100

    end chapter on Twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,11 +3,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,6 +23,15 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response("Future page to show article: ${slug}");
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
